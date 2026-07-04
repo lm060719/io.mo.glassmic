78a5450b69df714bad7f41eed392b95bf6cd1b40
lm060719 <limo060719@gmail.com>
2026-07-04 17:35:59 +0800

    版本号+1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "io.mo.glassmic"
         minSdk = 29
         targetSdk = 35
-        versionCode = 6
-        versionName = "1.2.1"
+        versionCode = 7
+        versionName = "1.2.5"
         resourceConfigurations += listOf("zh-rCN", "en")
     }
 
